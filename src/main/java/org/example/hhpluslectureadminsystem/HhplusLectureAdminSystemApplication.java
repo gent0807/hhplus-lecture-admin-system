bfce3b6e97e9f51ed379e7c4516f2131d934dbe4
gent0807 <theloopholesnk@gmail.com>
2024-12-23 10:23:24 +0900

    [Init]: First Setting, First commit

---
+++ src/main/java/org/example/hhpluslectureadminsystem/HhplusLectureAdminSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.hhpluslectureadminsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusLectureAdminSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusLectureAdminSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hhpluslectureadminsystem/HhplusLectureAdminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hhpluslectureadminsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusLectureAdminSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
