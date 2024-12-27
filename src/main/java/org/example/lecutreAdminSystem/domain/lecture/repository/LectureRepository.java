7d014482bbb4e8991b6159a9a78594e8eab86953
gent0807 <theloopholesnk@gmail.com>
2024-12-28 08:19:26 +0900

    [Fix]: 특강 조회의 날짜, 시간 데이터 이용 방식 변경
    - 데이터베이스 쿼리를 이용해서 시간대 조회 조건에 맞는 강의 리스트를 먼저
      조회하고 그 다음 날짜 기준으로 filter를 건다.

--- src/main/java/org/example/lecutreAdminSystem/domain/lecture/repository/LectureRepository.java
+++ src/main/java/org/example/lecutreAdminSystem/domain/lecture/repository/LectureRepository.java
@@ -1,14 +1,14 @@
 package org.example.lecutreAdminSystem.domain.lecture.repository;
 
 import org.example.lecutreAdminSystem.domain.lecture.entity.Lecture;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface LectureRepository {
     List<Lecture> findAll();
 
-    List<Lecture> findByStartDateLessThanEqualAndEndDateGreaterThanEqualAndStartTimeLessThanEqualAndEndTimeGreaterThanEqual(String startDate, String endDate, String startTime, String endTime);
+    List<Lecture> findByStartTimeGreaterThanEqualAndEndTimeLessThanEqual(LocalDateTime startTime, LocalDateTime endTime);
 
     Lecture findById(Long lectureId);
 
